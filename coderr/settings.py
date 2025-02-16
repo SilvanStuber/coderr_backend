51955013a27292de07c07d0c2ed83e816b2cdd24
Silvan Stuber <140338185+SilvanStuber@users.noreply.github.com>
2025-02-16 13:09:39 +0100

    SECURE_SSL_REDIRECT

--- coderr/settings.py
+++ coderr/settings.py
@@ -55,14 +55,15 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware', 
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ]
+
 
 CORS_ALLOW_ALL_ORIGINS = False
 
@@ -70,6 +71,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5500",
     "https://gc.silvanstuber.ch",
 ]
+
+SECURE_SSL_REDIRECT = False
 
 CORS_ALLOW_CREDENTIALS = True
 
